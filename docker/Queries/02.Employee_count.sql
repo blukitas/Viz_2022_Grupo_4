90742d2c6f697486e07aa1e67b9f137efc14b696
blukitas <lucasbertolini@otmail.com>
2022-05-31 23:50:17 -0300

    Employe count + graph

--- docker/Queries/02.Employee_count.sql
+++ docker/Queries/02.Employee_count.sql
@@ -3,11 +3,11 @@
 -- How many distinct employees in a jobid, distinct by day
   select to_char(cast("timestamp" as "timestamp"), 'YYYY-MM-DD') as ts 
        , j.employerid
-       , count(distinct(participantid))
+       , count(distinct(participantid)) employees_count
 	from activitylogs a
-  	  inner join jobs j on j.jobid = cast(a.jobid as varchar)
+  	  inner join jobs j on a.jobid = cast(j.jobid as varchar)
 	where to_char(cast("timestamp" as "timestamp"), 'MM') = '03'
-      and j.employerid  = '868'
+        and j.employerid  = '868'
 	group by to_char(cast("timestamp" as "timestamp"), 'YYYY-MM-DD') 
            , j.employerid
     order by to_char(cast("timestamp" as "timestamp"), 'YYYY-MM-DD') asc
