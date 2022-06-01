f8347bcaae322767ded213e7d09201166bce0882
blukitas <lucasbertolini@otmail.com>
2022-06-01 00:23:41 -0300

    Add current mode queries

--- docker/Queries/03.financial_status_over_time.sql
+++ docker/Queries/03.financial_status_over_time.sql
@@ -11,12 +11,35 @@ with universo as (
 select ts 
 	 , financialstatus 
 	 , count(distinct(participantid))
-	 , min(availablebalance)
-	 , avg(availablebalance)
-	 , max(availablebalance)
-	 , min(dailyfoodbudget)
-	 , avg(dailyfoodbudget)
-	 , max(dailyfoodbudget)
+	 , min(availablebalance) as min_availablebalance
+	 , avg(availablebalance) as avg_availablebalance
+	 , max(availablebalance) as max_availablebalance
+	 , min(dailyfoodbudget) as min_dailyfoodbudget
+	 , avg(dailyfoodbudget) as avg_dailyfoodbudget
+	 , max(dailyfoodbudget) as max_dailyfoodbudget
 from universo
 group by ts 
-	 , financialstatus 
+	 , financialstatus ;
+	 
+/*
+ * Current modes:
+ * Transport
+ * AtHome
+ * AtRecreation
+ * AtRestaurant
+ * AtWork
+ */
+with participant_current_mode as (	 
+	select to_char(cast("timestamp" as "timestamp"), 'YYYY-MM-DD') as ts 
+	 	 , a.currentmode
+		 , participantid
+		 , count(1) as mode_count
+	 from activitylogs a 
+	 group by to_char(cast("timestamp" as "timestamp"), 'YYYY-MM-DD')
+	 	 	, a.currentmode
+	 		, participantid 
+)
+select ts, p.currentmode, avg(p.mode_count) 
+from participant_current_mode p
+where p.currentmode = 'AtWork'
+group by ts, p.currentmode
