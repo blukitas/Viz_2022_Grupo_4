46107a206a9463b00583531939daeaf59fe68d8e
blukitas <lucasbertolini@otmail.com>
2022-06-11 15:23:24 -0300

    time spend in minutes

--- docker/SQL_post/27.add_tspend.sql
+++ docker/SQL_post/27.add_tspend.sql
@@ -2,3 +2,9 @@ alter table activitylogs add column ts_spend varchar null;
 
 update activitylogs 
 set ts_spend = ts_max::timestamp - ts_min::timestamp;
+
+
+alter table activitylogs add column ts_spend_minutes int null;
+
+update activitylogs 
+set ts_spend_minutes = EXTRACT(hour from "ts_spend"::time) * 60 + EXTRACT(minute from "ts_spend"::time) + 5;
