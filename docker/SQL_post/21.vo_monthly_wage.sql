3557829d0a583b6a028102cbcefe4a84acf649d3
blukitas <lucasbertolini@otmail.com>
2022-06-12 16:15:09 -0300

    Avg to monthly wages

--- docker/SQL_post/21.vo_monthly_wage.sql
+++ docker/SQL_post/21.vo_monthly_wage.sql
@@ -8,7 +8,11 @@ with monthly_spend as (
 		sum(case when category = 'Wage' then amount else 0 end) as wage_amt,
 		sum(case when category = 'Food' then amount else 0 end) as food_amt,
 		sum(case when category = 'Education' then amount else 0 end) as education_amt,
-		sum(case when category = 'Shelter' then amount else 0 end) as shelter_amt 
+		sum(case when category = 'Shelter' then amount else 0 end) as shelter_amt,
+		avg(case when category = 'Wage' then amount else 0 end) as wage_avg,
+		avg(case when category = 'Food' then amount else 0 end) as food_avg,
+		avg(case when category = 'Education' then amount else 0 end) as education_avg,
+		avg(case when category = 'Shelter' then amount else 0 end) as shelter_avg 
 	from financialjournal a
 	group by to_char(cast(a."timestamp" as date),'yyyy-MM'), 
 			 participantid
@@ -22,7 +26,14 @@ select
 	ROUND(shelter_amt::numeric, 2) as shelter_amt,
 	ROUND(wage_amt::numeric, 2) as earning_amt,
 	ROUND((food_amt + education_amt + shelter_amt)::numeric, 2) as spend_amt,
-	ROUND((wage_amt - food_amt + education_amt + shelter_amt)::numeric, 2) as balance_amt
+	ROUND((wage_amt - food_amt + education_amt + shelter_amt)::numeric, 2) as balance_amt,
+	ROUND(wage_avg::numeric, 2) as wage_avg,
+	ROUND(food_avg::numeric, 2) as food_avg,
+	ROUND(education_avg::numeric, 2) as education_avg,
+	ROUND(shelter_avg::numeric, 2) as shelter_avg,
+	ROUND(wage_avg::numeric, 2) as earning_avg,
+	ROUND((food_avg + education_avg + shelter_avg)::numeric, 2) as spend_avg,
+	ROUND((wage_avg - food_avg + education_avg + shelter_avg)::numeric, 2) as balance_avg
   into vo_monthly_wage
 from monthly_spend ms;
 
