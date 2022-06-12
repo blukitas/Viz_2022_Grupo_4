0266be97dda9a70e3955adea64de92d1f677ed47
blukitas <lucasbertolini@otmail.com>
2022-06-12 13:03:06 -0300

    date with dateparts

--- docker/SQL_post/24.vo_date.sql
+++ docker/SQL_post/24.vo_date.sql
@@ -2,6 +2,21 @@
 drop table if exists vo_date;
 
 create table vo_date as
-    select left(ts_min,10) as dt
-      from activitylogs a 
-      group by left(ts_min,10);
+with dates as (
+  select 
+  		left(ts_min,10) as dt,
+  		left(ts_min,10)::date as dt_date
+    from activitylogs a 
+    group by left(ts_min,10)
+)
+select dt,
+	   dt_date,
+	   EXTRACT(year FROM dt_date) dt_year,
+	   EXTRACT(month FROM dt_date) dt_month,
+	   EXTRACT(QUARTER FROM dt_date) dt_quater,
+	   EXTRACT(WEEK FROM dt_date) dt_week_of_year,
+	   EXTRACT(day FROM dt_date) dt_day,
+	   EXTRACT(DOW FROM dt_date) dt_day_of_week,
+	   EXTRACT(DOY FROM dt_date) dt_day_of_year,
+	   EXTRACT(EPOCH FROM dt_date) dt_epoch
+  from dates;
